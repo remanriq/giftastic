72a079300d038b583e8f2a5edd7f33befb01a68d
Rosa Manriquez <rosamanriquez@Rosas-MacBook-Pro.local>
2017-06-05 17:22:21 -0700

    render buttons

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,25 +1,33 @@
 $("button").on('click', function(){
-	console.log('BUTTON CLICKED!');
-})
-		var musicians = ["selena","lorde","grouplove","bob marley","lauryn hill","bruno mars","shakira","j.cole","metallica","the ramones"];
+	var music = $(this).attr("data-search");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + music + "&api_key=dc6zaTOxFJmzC&limit=10";
+	$.ajax({
+		url:queryURL,
+		method:'GET'})
+	.done(function(response){
+	for ( var i=0 ; i < response.data.length; i++){
+		var p = $('<p>').text('Rating:', response.data[i].rating);
+		//console.log(response);
+		var musicDiv = $('<div>');
+		var musicImage = $('<img>');
+		musicImage.attr('src', response.data[i].images.fixed_height.url)
+						;
+			musicDiv.append(p);
+			musicDiv.append(musicImage);
+			$('#musicView').prepend(musicDiv);
 
+		}
 
-		function displayMusicInfo(){
-			var musician = $(this).attr('data-search');
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + music + "&api_key=dc6zaTOxFJmzC&limit=10";
+	})
 
+});
 
-		$.ajax({
-			url: queryURL, 
-			method: 'GET'})
-	     .done(function(response);
-
-     var results = response.data;
-	  $(".gif").on("click", function(){
+	
+	 $("#musicview").on("click", function(){
             
-             var state = $(this).attr("data-state"); 
+            var state = $(this).attr("data-state"); 
 
-             var movieDiv = $("<div class='movie'>");
+             var MusicDiv = $("<div class='music'>");
 
              if (state === "still") {
                     
@@ -31,9 +39,45 @@ $("button").on('click', function(){
                             
             $(this).attr("src", $(this).data("still"));
                     
-           $(this).attr("data-state", "still");             }    
+           $(this).attr("data-state", "still"); 
+}
+
+})
+      
+
+        function renderButtons() {
+	$("#buttonsView").empty();
+for (var i = 0; i < response.data.length; i++){
+var a = $('<button>');
+a.addClass("music");
+a.attr("data-search", music[i]);
+a.text(music[i]);
+$("#buttonsView").append(a);
+
+}
+	
+$("#addmusic").on("click", function(event){
+event.preventDefault();
+
+var music = $("#musicinput").val().trim();
+music.push(music);
+
+renderButtons();
+
+         
+     	})
+     }
+	
+		
+
+
+
+
+
         
-       });
+
+
+		
      
 
 
